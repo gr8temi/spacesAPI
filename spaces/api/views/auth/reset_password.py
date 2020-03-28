cf48f3247c7a0a2f772fa9b85e4f86c10d6e3583
codeEmpress1 <halof.halima@gmail.com>
2020-03-28 21:40:44 +0100

    create endpoint to user to reset password

--- spaces/api/views/auth/reset_password.py
+++ spaces/api/views/auth/reset_password.py
@@ -6,31 +6,20 @@ from ...models.user import User
 
 
 class ResetPassword(APIView):
-    # pass
+
     def post(self, request):
         data = request.data
 
         password = data['password']
-        # print(data['token'], 'wwwwwwwww')
-        # print(password)
-        # confirmpassword = data['confirmpassword']
-        # if password:
         token = data['token']
-
-        # if len(password) < 8:
-        #     return Response({"message": "Invalid Password, password length must contain 8 characters or more"}, status=status.HTTP_400_BAD_REQUEST)
-
-        result = User.objects.get(token=token).first()
-        print(result)
+        result = User.objects.get(token=token)
+        
         if result:
-            user = result[0]
-            print(user)
+            user = result
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             user.password = hashed_password
             user.save()
-            message = "Your password has been successfully reset"
-            return Response({message: message}, status=status.HTTP_200_OK)
-        else:
-            return Response({"User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        # else:
-        #     return Response("Both fields muust be the same")
+            message = "Your password has successfully been reset"
+            return Response({"message": message}, status=status.HTTP_200_OK)
+        
+        return Response({"User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
