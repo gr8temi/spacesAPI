9f7632d71ba03e4d42593f44d387950e0bd94710
codeEmpress1 <halof.halima@gmail.com>
2020-03-28 22:28:16 +0100

    update app url

--- spaces/api/urls.py
+++ spaces/api/urls.py
@@ -4,17 +4,16 @@ from .views.auth import login
 from .views.add_space import CreateSpace
 
 
-from .views.auth import login, forgot_password, reset_password
+from .views.auth import forgot_password, reset_password, verify_email
 
 urlpatterns = [
 
     # register other routes here ...
     path('spaces/', CreateSpace.as_view(), name="space"),
 
-    # match route that has not been registered above
+    # path('auth/login/', login.UserLogin.as_view(), name='login'),
+    path('auth/verify-email/', verify_email.VerifyEmail.as_view(), name="verify-email"),
+    path('auth/forgot-password/', forgot_password.ForgotPassword.as_view(), name="forgot-password"),
+    path('auth/reset-password/', reset_password.ResetPassword.as_view(), name = 'reset-password'),
     re_path(r'^(?:.*)$', notfound),
-
-    path('auth/login/', login.UserLogin.as_view(), name='login'),
-    path('auth/forgot_password/', forgot_password.ForgotPassword.as_view(), name='forgot_password'),
-    path('auth/reset_password/', reset_password.ResetPassword.as_view(), name = 'reset_password')
 ]
