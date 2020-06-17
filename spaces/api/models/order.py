3c9a65c10f0ffb6dab47c941d37d44e9490b8805
codeEmpress1 <halof.halima@gmail.com>
2020-06-17 12:47:34 +0100

    update booking and reservation endpoint

--- spaces/api/models/order.py
+++ spaces/api/models/order.py
@@ -3,16 +3,29 @@ from django.db import models
 from .user import User
 from .order_type import OrderType
 from .spaces import Space
+from django.contrib.postgres.fields.jsonb import JSONField as JSONBField
+from django.contrib.postgres.fields import ArrayField, JSONField
+# from jsonfield import JSONField
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils import timezone
+
 
 class Order(models.Model):
     orders_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    usage_start_date = models.DateField()
-    usage_end_date = models.DateField()
+    amount = models.CharField(max_length=50, default=0)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    company_email = models.CharField(max_length=100, default="one@gmail.com")
+    no_of_guest = models.IntegerField()    
+    extras = JSONBField(null=True, blank=True, default=dict, encoder=DjangoJSONEncoder)
+    usage_start_date = models.DateTimeField(default=timezone.now)
+    usage_end_date = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=30)
-    transaction_code = models.CharField(max_length=30)
+    transaction_code = models.CharField(max_length=30, null=True)
     order_code = models.CharField(max_length=30)
     order_type = models.ForeignKey(OrderType, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
     order_time = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField( auto_now_add=True)
--- spaces/api/tests/test_booking.py
+++ spaces/api/tests/test_booking.py
@@ -5,7 +5,7 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.utils import timezone
 from datetime import date, timedelta, datetime
-
+import json
 from ..views.booking import Booking
 from ..models.spaces_category import SpaceCategory
 from ..models.agent import Agent
@@ -13,12 +13,13 @@ from ..models.spaces import Space
 from ..models.user import User
 from ..models.customer import Customer
 from ..models.order_type import OrderType
+from ..models.availabilities import Availability
+from ..models.extras import Extra
 from ..helper.helper import order_code
-from .mock_data.space_data import space_creation_data, space_category_data
-from .mock_data.order_data import order_data1, order_data2, order_data3, order_data4
+from .mock_data.space_data import space_creation_data, space_category_data, extra1, extra2, availability1, availability2, availability3_1, availability3_2, availability3_3, availability3_4, availability3_5, availability3_6, availability3_7, hourly_space_data, monthly_space_data, yearly_space_data
+from .mock_data.order_data import order_data1, order_data2, order_data3, order_data4, order_extra
 from .mock_data.registration_data import user_registration_data, agent_registration_data, customer_registration_data, customer_login_data
 from .mock_data.order_data import order_type_booking, order_data,order_type_reservation
-
 
 
 class TestBooking(APITestCase):
@@ -35,8 +36,21 @@ class TestBooking(APITestCase):
         # create order_type
         self.order_type1 = OrderType.objects.create(**order_type_booking())
         self.order_type2 = OrderType.objects.create(**order_type_reservation())
-        # cretae space
+        # create space
         self.space = Space.objects.create(**space_creation_data(), agent=self.agent, space_category=self.space_category_id)
+        self.hourlyspace = Space.objects.create(**hourly_space_data(), agent=self.agent, space_category=self.space_category_id)
+        self.monthlyspace = Space.objects.create(**monthly_space_data(), agent=self.agent, space_category=self.space_category_id)
+        self.yearlyspace = Space.objects.create(**yearly_space_data(), agent=self.agent, space_category=self.space_category_id)
+
+        self.availability = Availability.objects.create(**availability1())
+        self.availability2 = Availability.objects.create(**availability2())
+        self.availability3_1 = Availability.objects.create(**availability3_1())
+        self.availability3_2 = Availability.objects.create(**availability3_2())
+        self.availability3_3 = Availability.objects.create(**availability3_3())
+        self.availability3_4 = Availability.objects.create(**availability3_4())
+        self.availability3_5 = Availability.objects.create(**availability3_5())
+        self.availability3_6 = Availability.objects.create(**availability3_6())
+        self.availability3_7 = Availability.objects.create(**availability3_7())
 
         self.data = {**customer_login_data()}
         self.url = reverse('login')
@@ -49,26 +63,52 @@ class TestBooking(APITestCase):
     def test_booking1(self):
         data1 = {
             **order_data1(),
+            **order_extra(),
             'space': self.space.space_id,
             'order_type': self.order_type1
         }
 
         data2 = {
             **order_data2(),
+            **order_extra(),
             'space': self.space.space_id,
             'order_type': self.order_type1
         }
 
         data3 = {
             **order_data3(),
+            **order_extra(),
             'space': self.space.space_id,
             'order_type': self.order_type1
         }
 
-    
+        data4 = {
+            **order_data3(),
+            **order_extra(),
+            'space': self.hourlyspace.space_id,
+            'order_type': self.order_type1
+
+        }
+
+        data5 = {
+            **order_data3(),
+            **order_extra(),
+            'space': self.monthlyspace.space_id,
+            'order_type': self.order_type1
+        }
+
+        data6 = {
+            **order_data3(),
+            **order_extra(),
+            'space': self.yearlyspace.space_id,
+            'order_type': self.order_type1
+        }
         response1 = self.client.post(self.url2, data1, HTTP_AUTHORIZATION=self.header)
         response2 = self.client.post(self.url2, data2, HTTP_AUTHORIZATION=self.header)
         response3 = self.client.post(self.url2, data3, HTTP_AUTHORIZATION=self.header)
+        response4 = self.client.post(self.url2, data4, HTTP_AUTHORIZATION=self.header)
+        response5 = self.client.post(self.url2, data5, HTTP_AUTHORIZATION=self.header)
+        response6 = self.client.post(self.url2, data6, HTTP_AUTHORIZATION=self.header)
     
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response1.data['message'], "Order completed")
@@ -77,16 +117,26 @@ class TestBooking(APITestCase):
         self.assertEqual(response2.data['message'], "Order completed")
 
         self.assertEqual(response3.status_code, 503)
-        self.assertEqual(response3.data['message'], "Space unavailable, pick a date later than 2020-04-23 or check another space")
+        self.assertEqual(response3.data['message'], "Space unavailable, pick a date later than 2020-04-28 15:00:00 or check another space")
+
+        self.assertEqual(response4.status_code, 200)
+        self.assertEqual(response4.data['message'], "Order completed")
+
+        self.assertEqual(response5.status_code, 200)
+        self.assertEqual(response5.data['message'], "Order completed")
+
+        self.assertEqual(response6.status_code, 200)
+        self.assertEqual(response6.data['message'], "Order completed")
 
     def test_reservation(self):
 
-        data4 = {
+        data = {
         **order_data4(),
+        **order_extra(),
         'space': self.space.space_id,
         'order_type': self.order_type2
         }
-        response1 = self.client.post(self.url3, data4, HTTP_AUTHORIZATION=self.header)
+        response1 = self.client.post(self.url3, data, HTTP_AUTHORIZATION=self.header)
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response1.data["message"], f"{self.space.name} reserved from 2020-04-27 to 2020-04-29")
         
