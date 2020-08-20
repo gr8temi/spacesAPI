8fc3ee155b06bf02db78d6c71dd28423d23af75a
Adams Temidire <gr8temi@gmail.com>
2020-08-21 00:00:08 +0100

    create edit space endpoint

--- spaces/api/views/space.py
+++ spaces/api/views/space.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -15,6 +16,8 @@ from ..serializers.extra import ExtraSerializer
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
 import os
+from rest_framework.generics import UpdateAPIView
+from ..permissions.is_agent_permission import UserIsAnAgent
 
 
 class Spaces(APIView):
@@ -36,7 +39,7 @@ class SingleSpace(APIView):
         try:
             return Availability.objects.filter(space=space_id)
         except Exception as err:
-            
+
             return False
 
     def get_extra(self, space_id):
@@ -44,7 +47,7 @@ class SingleSpace(APIView):
 
             return Extra.objects.filter(space=space_id)
         except Exception as err:
-            
+
             return False
 
     def get_booked(self, space_id):
@@ -52,7 +55,7 @@ class SingleSpace(APIView):
 
             return Order.objects.filter(space=space_id)
         except Exception as err:
-            
+
             return False
 
     def get(self, request, space_id):
@@ -61,14 +64,22 @@ class SingleSpace(APIView):
         if space:
             availability = self.get_availability(space.name)
             extras = self.get_extra(space.name)
-            
+
             booked = self.get_booked(space_id)
-        
+
             space_serializer = SpaceSerializer(space)
             extra_serializer = ExtraSerializer(extras, many=True)
             availability_serializer = AvailabilitySerializer(
                 availability, many=True)
             bookedSerializer = OrderSerializer(booked, many=True)
-            return Response({"message": "Space fetched successfully", "payload": {**space_serializer.data, "availability": availability_serializer.data, "extras": extra_serializer.data, "booked_date":bookedSerializer.data}}, status=status.HTTP_200_OK)
+            return Response({"message": "Space fetched successfully", "payload": {**space_serializer.data, "availability": availability_serializer.data, "extras": extra_serializer.data, "booked_date": bookedSerializer.data}}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "Error fetching space, Space does not Exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EditSpace(UpdateAPIView):
+
+    queryset = Space.objects.all()
+    serializer_class = SpaceSerializer
+    lookup_field = 'space_id'
+    permission_classes = [IsAuthenticated & UserIsAnAgent]
