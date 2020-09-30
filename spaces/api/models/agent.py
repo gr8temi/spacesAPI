b9583d1dca35510f8a0a859879a584c2f2792e19
Adams Temidire <gr8temi@gmail.com>
2020-09-30 14:07:22 +0100

    change agent details

--- spaces/api/models/agent.py
+++ spaces/api/models/agent.py
@@ -4,14 +4,18 @@ from .user import User
 
 
 class Agent(models.Model):
-    agent_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    agent_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    business_name = models.CharField( max_length=50)
-    office_address =models.CharField( max_length=50)
+    business_name = models.CharField(max_length=50)
+    office_address = models.CharField(max_length=50)
+    account_number = models.CharField(max_length=10, blank=True)
+    account_name = models.CharField(max_length=50, blank=True)
+    bank = models.CharField(max_length=256, blank=True)
+    document = models.CharField(max_length=256, blank=True)
     validated = models.BooleanField(default=False)
-    updated_at = models.DateTimeField( auto_now=True)
-    created_at = models.DateTimeField( auto_now_add=True)
-    
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.business_name
-    
