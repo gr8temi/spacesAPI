234e0e2a55449c15a1ac9b16f552fd3bb039e9e4
Adams Temidire <gr8temi@gmail.com>
2020-12-20 11:18:38 +0100

    fix monthly booking

--- spaces/api/serializers/customer.py
+++ spaces/api/serializers/customer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ..models.customer import Customer
-
+from api.serializers.user import UserSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +10,8 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializerDetail(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Customer
-        fields = "__all__"
+        fields = ['customer_id', 'user']
         depth = 1
