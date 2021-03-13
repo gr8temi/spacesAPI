ada6d0260d094bc37e818891c29d6fa6d745ab86
Adejoke <ogunyinkajoke@gmail.com>
2021-03-13 15:39:04 +0100

    feat subscription admin view

--- spaces/api/admin.py
+++ spaces/api/admin.py
@@ -15,7 +15,6 @@ from api.helper.send_cancellation_email import CancellationActions
 from django.urls import path, reverse
 from django.shortcuts import redirect
 from .resources.spaces_resource import SpaceResource
-import uuid
 from .models.order import Order
 from .models.spaces import Space
 from .models.space_type import SpaceType
@@ -23,8 +22,10 @@ from .models.spaces_category import SpaceCategory
 from .models.agent import Agent
 from .models.user import User
 from .models.order_type import OrderType
+from .models.subscription import Subscription
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from .resources.order_resource import OrderResource
+from .resources.subscription_resource import SubscriptionResource
 
 # from api.models.availabilities import Availability
 models = apps.get_models()
@@ -139,6 +140,12 @@ class OrderAdmin(ExportMixinAdmin):
     list_filter = (
         ("order_time", DateRangeFilter), ('order_time', DateTimeRangeFilter), ('order_type')
     )
+
+@admin.register(Subscription)
+class SubscriptionAdmin(ExportMixinAdmin):
+    resource_class = SubscriptionResource
+    list_display = ('subscription', 'subscription_type', 'subscription_plan', 'cost')
+    list_filter = ('subscription_title', 'subscription_type', 'subscription_plan')
 
 @admin.register(Cancellation)
 class CancellationAdmin(ExportMixinAdmin):
