b70c1ac16a6b271472650f606ac59c84a4b4b522
Adams Temidire <gr8temi@gmail.com>
2021-04-23 07:28:55 +0100

    fix ratings background task

--- spaces/api/models/ratings.py
+++ spaces/api/models/ratings.py
@@ -2,13 +2,15 @@ import uuid
 from django.db import models
 from .user import User
 from .spaces import Space
+from .order import Order
 
 class Rating(models.Model):
     rating_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     ratings = models.FloatField()
     comment = models.TextField()
-    user = models.ForeignKey(User,verbose_name='Customer', max_length=250, default=None, on_delete=models.CASCADE)
+    user = models.CharField(verbose_name='Customer', max_length=250, default=None)
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.space.name
