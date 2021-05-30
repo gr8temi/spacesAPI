6e10f4ee25d04ddf589069a0880ee4003d59e994
Adams Temidire <gr8temi@gmail.com>
2021-05-30 08:38:18 +0100

    add send message

--- spaces/api/views/paystack.py
+++ spaces/api/views/paystack.py
@@ -1,11 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.core.mail import send_mail
+
 
 class PaystackHooks(APIView):
-
     def post(self, request):
         data = request.body
         event = data.event
-        print({"event":event,"data":data})
-        return Response({"message":"done"}, status=status.HTTP_200_OK)
+        print({"event": event, "data": data})
+        send_mail(
+            "Subject here",
+            f"Here is the message.{event}",
+            "damstemii@gmail.com",
+            ["gr8temi@gmail.com"],
+            fail_silently=False,
+        )
+        return Response({"message": "done"}, status=status.HTTP_200_OK)
